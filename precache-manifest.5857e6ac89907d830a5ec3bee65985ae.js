c0857ab3ace05f8a712f11bdfd86d89df6342480
roman <romagladyshev@gmail.com>
2019-11-14 22:58:48 +0600

    Init

---
+++ precache-manifest.5857e6ac89907d830a5ec3bee65985ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6340b13a6cad79b515e921bd042159d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f43f5d1d7ffc8824d4d",
+    "url": "/static/css/2.55521911.chunk.css"
+  },
+  {
+    "revision": "d7efdba070aeb1113019",
+    "url": "/static/css/main.a15c1319.chunk.css"
+  },
+  {
+    "revision": "0f43f5d1d7ffc8824d4d",
+    "url": "/static/js/2.43740d4e.chunk.js"
+  },
+  {
+    "revision": "d7efdba070aeb1113019",
+    "url": "/static/js/main.bd6d160d.chunk.js"
+  },
+  {
+    "revision": "67b49524e683e87192e9",
+    "url": "/static/js/runtime-main.ed14a1f1.js"
+  }
+]);
